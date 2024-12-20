b3b81e38a9cb97cf9e9012b8ce8cc70ed652ddb9
sdn-1 <860491243@qq.com>
2024-12-20 21:10:30 +0800

    第二次提交，基本实现服务器和客户端的聊天连接

---
+++ mainwindow.cpp
@@ -0,0 +1,94 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QHostAddress>
+#include <QJsonObject>
+#include <QJsonObject>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    ui->stackedWidget->setCurrentWidget(ui->loginPage);
+
+    m_chatClient = new ChatClient(this);
+
+    connect(m_chatClient, &ChatClient::connected, this, &MainWindow::connectedToServer);
+    //connect(m_chatClient, &ChatClient::messageReceived, this, &MainWindow::messageReceived);
+    connect(m_chatClient, &ChatClient::jsonReceived, this, &MainWindow::jsonReceived);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_loginButton_clicked()
+{
+    m_chatClient->connectToServer(QHostAddress(ui->serverEdit->text()), 1967);
+
+}
+
+
+void MainWindow::on_sayButton_clicked()
+{
+    if (!ui->sayLineEdit->text().isEmpty())
+        m_chatClient->sendMessage(ui->sayLineEdit->text());
+}
+
+
+void MainWindow::on_logoutButton_clicked()
+{
+    // ui->stackedWidget->setCurrentWidget(ui->loginPage);
+    // m_chatClient->sendMessage(ui->usernameEdit->text(), "login");
+    m_chatClient->disconnectFromHost();
+    ui->stackedWidget->setCurrentWidget(ui->loginPage);
+}
+
+void MainWindow::connectedToServer()
+{
+    ui->stackedWidget->setCurrentWidget(ui->chatPage);
+    m_chatClient->sendMessage(ui->usernameEdit->text(), "login");
+}
+
+void MainWindow::messageReceived(const QString &sender, const QString &text)
+{
+    ui->roomTexitEdit->append(QString("%1 : %2").arg(sender).arg(text));
+}
+
+void MainWindow::jsonReceived(const QJsonObject &docObj)
+{
+    const QJsonValue typeVal = docObj.value("type");
+    if (typeVal.isNull() || !typeVal.isString())
+        return;
+    if (typeVal.toString().compare("message", Qt::CaseInsensitive) == 0) {
+        const QJsonValue textVal = docObj.value("text");
+        const QJsonValue senderVal = docObj.value("sender");
+
+        if (textVal.isNull() || !textVal.isString())
+            return;
+
+        if (senderVal.isNull() || !senderVal.isString())
+            return;
+
+        // const QString text = textVal.toString().trimmed();
+        // if (text.isEmpty())
+        //     return;
+
+        messageReceived(senderVal.toString(), textVal.toString());
+
+    } else if (typeVal.toString().compare("newuser", Qt::CaseInsensitive) == 0) {
+        const QJsonValue usernameVal = docObj.value("username");
+        if (usernameVal.isNull() || !usernameVal.isString())
+            return;
+
+        userJoined(usernameVal.toString());
+    }
+}
+
+void MainWindow::userJoined(const QString &user)
+{
+    ui->userListWidget->addItem(user);
+}
+
